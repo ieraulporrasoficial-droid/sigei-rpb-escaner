1a294da91f8c8a80c1f4e86b7d1728125dd0a86c
ieraulporrasoficial-droid <ieraulporrasoficial@gmail.com>
2026-07-22 08:34:02 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -41,39 +41,6 @@ function renderizarAplicacion() {
 }
 
 
-function aplicarTema() {
-
-  const configuracion = SIGEI.CONFIG?.datos;
-
-  if (!configuracion?.colores) {
-    return;
-  }
-
-  const colores = configuracion.colores;
-
-  document.documentElement.style.setProperty(
-    "--color-principal",
-    colores.principal || "#0FA958"
-  );
-
-  document.documentElement.style.setProperty(
-    "--color-secundario",
-    colores.secundario || "#FFD600"
-  );
-
-  document.documentElement.style.setProperty(
-    "--color-fondo",
-    colores.fondo || "#FFFFFF"
-  );
-
-  document.documentElement.style.setProperty(
-    "--color-texto",
-    colores.texto || "#222222"
-  );
-
-}
-
-
 function renderizarEncabezado() {
 
   const configuracion = SIGEI.CONFIG?.datos;
