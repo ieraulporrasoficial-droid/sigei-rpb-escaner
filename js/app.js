cffaaa76e65e3abb513954db74bd68e2f246333d
ieraulporrasoficial-droid <ieraulporrasoficial@gmail.com>
2026-07-21 23:10:25 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -32,52 +32,6 @@ const SIGEI = {
 };
 
 
-async function cargarConfiguracion() {
-
-  try {
-
-    const respuesta = await fetch(
-      SIGEI.API.BASE +
-      SIGEI.API.CONFIGURACION
-    );
-
-    if (!respuesta.ok) {
-      throw new Error(
-        `Error HTTP: ${respuesta.status}`
-      );
-    }
-
-    const datos = await respuesta.json();
-
-    if (!datos.exito) {
-      throw new Error(
-        "La API no devolvió una respuesta exitosa."
-      );
-    }
-
-    SIGEI.CONFIG = datos;
-
-    console.log(
-      "Configuración recibida:",
-      datos
-    );
-
-    renderizarAplicacion();
-
-  } catch (error) {
-
-    console.error(
-      "Error al cargar la configuración:",
-      error
-    );
-
-    SIGEI.ESTADO.error = error;
-
-  }
-
-}
-
-
 function renderizarAplicacion() {
 
   aplicarTema();
